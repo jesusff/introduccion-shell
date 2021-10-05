c22e5e1df1f2e9c5a9dce491218778e0e7cafa14
Jesus Fernandez <Jesus.Fernandez@unican.es>
2021-10-05 10:14:04 +0200

    Fix renombra.sh
    Improved to avoid removing extension with the tool basename, which
    also removes path.
    
    Also, add author header and download data automatically.

--- renombra.sh
+++ renombra.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# renombra.sh
+#
+# Script para renombrar ficheros pdb incluyendo la formula quimica en el
+# nombre.
+#
+# Autor: Jesus Fernandez (ifca.unican.es)
+# Creado: 2021-10-04
 
 function saca_formula(){
   fich=$1
@@ -9,11 +17,21 @@ function saca_formula(){
     | tr -d ' \n'
 }
 
+#
+# Descarga de datos
+#
+wget https://swcarpentry.github.io/shell-novice/data/shell-lesson-data.zip
+unzip -q shell-lesson-data.zip
+rm shell-lesson-data.zip
+cd shell-lesson-data
+#
+# Renombrado de ficheros
+#
 for fichero in data/pdb/*.pdb
 do
   echo -n "Renombrando ${fichero} -> ${nuevo_nombre} ..."
   formula=$(saca_formula ${fichero})
-  nuevo_nombre="$(basename ${fichero} .pdb)_${formula}.pdb"
+  nuevo_nombre="${fichero//\.pdb/}_${formula}.pdb"
   mv ${fichero} ${nuevo_nombre}
   echo "Done!"
 done

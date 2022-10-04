45d89f4dee21cab1b6de9e951045a60dc76668ca
Jesus Fernandez <jesusff@users.noreply.github.com>
2022-10-04 09:39:38 +0200

    Add header

--- renombra.sh
+++ renombra.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Script para renombrar ficheros de acuerdo a su formula quimica extraida del
+# contenido del fichero.
+#
+# Autor: Jesus Fernandez
+# Fecha: 2022-10-03
 
 function formula(){
   cat $1 | grep ATOM | awk '{print $3}' | uniq -c | awk '{print $2,$1}' | tr -d ' \n'
